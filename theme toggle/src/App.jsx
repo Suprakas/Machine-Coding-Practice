3fba4fcb19fa31cf23cf6c41ff33823cc9f950ff
SUPRAKASH BALLAV <Suprakashballav1997@gmail.com>
2026-08-09 14:50:27 +0530

    Theme Toggle is added

---
+++ theme toggle/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "light" || savedTheme === "dark" ? savedTheme : "light";
+  });
+
+  const handleThemeToggle = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  useEffect(()=>{
+localStorage.setItem("theme", theme)
+  },[])
+
+  return (
+    <div className={theme === "light" ? "light-theme" : "dark-theme"}>
+      <h1>Theme Toggle</h1>
+      <button onClick={handleThemeToggle}>Switch Theme</button>
+      <h2>Current theme: {theme}</h2>
+    </div>
+  );
+}
+
+export default App;
