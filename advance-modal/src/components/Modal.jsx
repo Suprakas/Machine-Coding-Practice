903503c5bcb7c4a3f3694e38345433cb8c90f5a8
SUPRAKASH BALLAV <Suprakashballav1997@gmail.com>
2026-08-31 11:06:03 +0530

    advance modal finished, adding keyboard events and applied how to prevent event propagation from modal to modal-overlay

--- advance-modal/src/components/Modal.jsx
+++ advance-modal/src/components/Modal.jsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react"
+
 export default function Modal ({handleCloseModal}) {
+
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if(e.key === "Escape"){
+                handleCloseModal()
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        }
+    }, [handleCloseModal])
+
     return (
-         <div className="modal-overlay">
-          <div className="modal">
+         <div className="modal-overlay" onClick={handleCloseModal}>
+          <div className="modal" onClick={(e) => e.stopPropagation()}>
             <button onClick={handleCloseModal}>Close Modal</button>
             <p>I am Modal conatainer</p>
           </div>
